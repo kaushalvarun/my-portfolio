07ca31dc692dc65e908084346b364a157ec488ef
KaushalCorp <kaushalvarunkv@gmail.com>
2025-01-20 20:10:38 +0530

    cgpa_update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,10 +13,10 @@ function Home() {
         <h2 style={{ margin: '0' }}>Hi, I'm</h2>
         <h1 className="main-heading" style={{ marginTop: '10px', paddingTop: '10px' }}>Varun Kaushal</h1>
         <p className="body-text">
-        I'm currently in my third year of BTech in Computer Science and Engineering at VIT University, Vellore. 
+        I'm in my third year of BTech in Computer Science and Engineering at VIT University, Vellore. 
         </p>
         <p className="body-text">
-        When it comes to tech, I'm super passionate about software development, especially app development and competitive coding. I also love exploring other fields like Web Development and Machine Learning whenever I get the chance. So far, I've developed a chatting app and a food delivery app, and I've got plenty more ideas in the pipeline!
+        When it comes to tech, I'm super passionate about software development, especially app development and competitive coding. I also love exploring other fields like Web Development and Machine Learning whenever I get the chance. So far, I've developed a ML powered movie recommender, and a food delivery app, and I've got plenty more ideas in the pipeline!
         </p>
         <p className="body-text">
         I'm a big believer in cloud computing, with certifications like AWS Cloud Practitioner and Microsoft Azure AI Fundamentals to back it up!
